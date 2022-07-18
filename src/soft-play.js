f424a4ae5cda84d780954e6a3f31d9143b3d27db
mbalabanov <marincomics@gmail.com>
2022-07-18 14:43:19 +0300

    Solves initial scope of exercise

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,6 +1,6 @@
 // do not change these lines
 
-function reset () {
+function reset() {
   adults = 0
   children = 0
 }
@@ -10,11 +10,35 @@ let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
+function enter(numAdults, numChildren) {
+  if (numAdults >= 1 && numChildren >= 1 && numAdults >= numChildren) {
+    adults += numAdults
+    children += numChildren
+    return true
+  }
+  return false
+}
+
+function leave(numAdults, numChildren) {
+  if (numAdults >= numChildren && numAdults <= adults && numChildren <= children && (adults - numAdults) >= (children - numChildren)) {
+    adults -= numAdults
+    children -= numChildren
+    return true
+  }
+  return false
+}
+
+function occupancy() {
+  return {
+    adults: adults,
+    children: children
+  }
+}
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined,
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
   reset: reset
 }
